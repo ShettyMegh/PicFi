cd309cdbcbea6b03f817045d26abb31293b4791f
Meghanath Shetty <68772102+MeghaNatham@users.noreply.github.com>
2022-04-18 15:47:57 +0530

    added thesholding

--- picFi.py
+++ picFi.py
@@ -2,19 +2,30 @@ import cv2 as cv;
 print("Welcome to PicFi")
 path = input("Enter Image Path\n")
 
+img = cv.resize(cv.imread(path),(450,600))
 
 flag = True;
 
 while flag:
-    filter = int(input("Choose the Filter \n 1.Grey  2.Histogram Processing  3.Thresholding 4.Exit\n"));
+    filter = int(input("Choose the Filter \n 1.Grey  2.Thresholding  3.Histogram Processing   4.Exit\n"));
     if filter == 1:
-        img = cv.imread(path,0)
-        cv.imshow("Grey Output",cv.resize(img,(450,550)))
+        grey = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+        cv.imshow("Input Image",img)
+        cv.imshow("Output Image",grey)
         cv.waitKey(0)
         cv.destroyAllWindows()
+
+    elif filter == 2:
+        grey = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+        threshVal = int(input("Enter Threshold Value: "))
+        #ret is boolean value that returns true if the frame is available
+        ret, threshImg = cv.threshold(grey, 120, 255, cv.THRESH_BINARY)
+        cv.imshow("Input Image",grey)
+        cv.imshow("Output Image",threshImg)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
     elif filter == 4:
         flag=False;
-
-
-# cv.imshow("Output",inpImg)
-# cv.waitKey(0)
+    else:
+        print("Wrong Input")
